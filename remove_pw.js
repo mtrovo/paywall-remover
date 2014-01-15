6a3180bdf6a1d16fa5cb7d636514c33924a9feac
Moises Trovo <moises.trovo@gmail.com>
2014-01-15 17:36:36 -0200

    versao mais atual para paywall da folha

--- remove_pw.js
+++ remove_pw.js
@@ -1,9 +1,12 @@
 // complete version for editing and stuff
 (function(){
   // select paywall image and paywall parent container
-  var img=document.querySelectorAll('[name=paywallimage]')[0]
-  var pw=img.parentNode.parentNode
-  pw.style.top='-1000px'
+  var imgs=document.querySelectorAll('[name=paywallimage]')
+  for(var i=0; i < imgs.length; i++){
+  	var pw = imgs[i], e=0;
+	  while((e=pw.parentNode).nodeName != 'BODY')pw=e;
+	  pw.style.top='-1000px';
+  }
 
   document.body.setAttribute('style', '')
 })();
